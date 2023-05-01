f2898842ad5df5baeb4cc4492f7391b459a03986
Luke Fabry <lf652@drexel.edu>
2023-05-01 18:37:46 -0400

    Fixed menu

---
+++ generator/generator/src/DropdownElement.js
@@ -0,0 +1,12 @@
+function Element ( props ) {
+    var content = props.name
+    return (
+        <div>
+            <div>
+                { content }
+            </div>
+        </div>
+    )
+}
+
+export { Element }
